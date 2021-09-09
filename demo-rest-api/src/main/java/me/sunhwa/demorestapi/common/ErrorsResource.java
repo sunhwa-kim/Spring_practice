942198f063a9fd3911297bbe606f2811d9a21af7
sunhwa-kim <ksh0810@gmail.com>
2021-09-09 18:14:54 +0900

    README

---
+++ demo-rest-api/src/main/java/me/sunhwa/demorestapi/common/ErrorsResource.java
@@ -0,0 +1,18 @@
+package me.sunhwa.demorestapi.common;
+
+import me.sunhwa.demorestapi.index.IndexController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.validation.Errors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class ErrorsResource extends EntityModel<Errors> {   //  Resource
+    public static EntityModel<Errors> modelOf(Errors errors) {
+        //        super(content, links);   // deprecated
+        EntityModel<Errors> errorsModel = EntityModel.of(errors);
+        errorsModel.add(linkTo(methodOn(IndexController.class).index()).withRel("index"));
+        return errorsModel;
+    }
+
+}
